6a4ee71363cebfb51c93ff3b43c4d168b3b9e517
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-07-19 13:40:28 +0200

    Remove all code except cppcheck

--- jenkins-scripts/lib/gazebo-base-default.bash
+++ jenkins-scripts/lib/gazebo-base-default.bash
@@ -33,42 +33,8 @@ apt-get update
 # Required stuff for Gazebo
 apt-get install -y ${BASE_DEPENDENCIES} ${GAZEBO_BASE_DEPENDENCIES} ${GAZEBO_EXTRA_DEPENDENCIES} ${EXTRA_PACKAGES}
 
-# Optional stuff. Check for graphic card support
-if ${GRAPHIC_CARD_FOUND}; then
-    apt-get install -y ${GRAPHIC_CARD_PKG}
-    # Check to be sure version of kernel graphic card support is the same.
-    # It will kill DRI otherwise
-    CHROOT_GRAPHIC_CARD_PKG_VERSION=\$(dpkg -l | grep "^ii.*${GRAPHIC_CARD_PKG}\ " | awk '{ print \$3 }')
-    if [ "\${CHROOT_GRAPHIC_CARD_PKG_VERSION}" != "${GRAPHIC_CARD_PKG_VERSION}" ]; then
-       echo "Package ${GRAPHIC_CARD_PKG} has different version in chroot and host system. Maybe you need to update your host" 
-       exit 1
-    fi
-fi
-
-# Step 2: configure and build
-
-# Normal cmake routine for Gazebo
-rm -rf $WORKSPACE/build $WORKSPACE/install
-mkdir -p $WORKSPACE/build $WORKSPACE/install
-cd $WORKSPACE/build
-CMAKE_PREFIX_PATH=/opt/ros/${ROS_DISTRO} cmake ${GZ_CMAKE_BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=/usr $WORKSPACE/gazebo
-make -j${MAKE_JOBS}
-make install
-. /usr/share/gazebo/setup.sh
-LD_LIBRARY_PATH=/opt/ros/${ROS_DISTRO}/lib make test ARGS="-VV" || true
-
 # Step 3: code check
 cd $WORKSPACE/gazebo
-# Workaround to avoid jenkins to close connection while waiting cppcheck to
-# finish, since no output is generated
-cat > keep_output.sh <<- DELIM3
-while true; do
-    echo "-"
-    sleep 30s
-done
-DELIM3
-sh keep_output.sh &
-K_PID=\$!
 sh tools/code_check.sh -xmldir $WORKSPACE/build/cppcheck_results || true
 kill -9 \$K_PID
 DELIM
