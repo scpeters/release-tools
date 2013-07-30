d9f393ce880441bbced8c4cb87e7171ef266d973
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-07-30 12:20:12 +0200

    Add instruction to handle code download

--- jenkins-scripts/lib/cppcheck-base.bash
+++ jenkins-scripts/lib/cppcheck-base.bash
@@ -3,6 +3,28 @@
 #stop on error
 set -e
 
-cd ${WORKSPACE}/code
-mkdir -p ${WORKSPACE}/code/build/cppcheck_results
-sh tools/code_check.sh -xmldir $WORKSPACE/build/cppcheck_results || true
+# Mercurial jenkins plugin can not handle variables for checking
+# source. Manual code to do that
+
+if [ -z ${SOFTWARE} ]; then
+    echo "Need SOFTWARE variable to be defined"
+    exit 1
+fi
+
+SOFTWARE_DIR=${WORKSPACE}/${SOFTWARE}
+
+if [ -d ${SOFTWARE_DIR} ]; then
+    hg clone https://bitbucket.org/osrf/$SOFTWARE ${SOFTWARE_DIR}
+else
+    cd ${SOFTWARE_DIR}
+    hg revert --all 
+    hg update --clean
+fi
+    
+cd ${SOFTWARE_DIR}
+hg update ${MERCURIAL_REVISION}
+
+# Run cpp check
+rm -fr ${WORKSPACE}/cppcheck_results
+mkdir -p ${WORKSPACE}/cppcheck_results
+sh tools/code_check.sh -xmldir $WORKSPACE/cppcheck_results || true
