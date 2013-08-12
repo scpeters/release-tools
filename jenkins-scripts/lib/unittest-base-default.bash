1de6531adc2134bad9f6ec25eeecfb2cd2f89e3d
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-08-12 18:47:53 +0200

    Support for running unit tests

---
+++ jenkins-scripts/lib/unittest-base-default.bash
@@ -0,0 +1,52 @@
+#!/bin/bash -x
+
+#stop on error
+set -e
+
+. ${SCRIPT_DIR}/lib/boilerplate_prepare.sh
+
+cat > build.sh << DELIM
+###################################################
+# Make project-specific changes here
+#
+set -ex
+
+# OSRF repository to get bullet
+apt-get install -y wget
+sh -c 'echo "deb http://packages.osrfoundation.org/drc/ubuntu ${DISTRO} main" > /etc/apt/sources.list.d/drc-latest.list'
+wget http://packages.osrfoundation.org/drc.key -O - | apt-key add -
+apt-get update
+
+# Step 1: install everything you need
+
+# Required stuff for Gazebo
+apt-get install -y ${BASE_DEPENDENCIES} ${GAZEBO_BASE_DEPENDENCIES} ${GAZEBO_EXTRA_DEPENDENCIES} ${EXTRA_PACKAGES}
+
+# ! To be removed
+# Optional stuff. Check for graphic card support
+if ${GRAPHIC_CARD_FOUND}; then
+    apt-get install -y ${GRAPHIC_CARD_PKG}
+    # Check to be sure version of kernel graphic card support is the same.
+    # It will kill DRI otherwise
+    CHROOT_GRAPHIC_CARD_PKG_VERSION=\$(dpkg -l | grep "^ii.*${GRAPHIC_CARD_PKG}\ " | awk '{ print \$3 }')
+    if [ "\${CHROOT_GRAPHIC_CARD_PKG_VERSION}" != "${GRAPHIC_CARD_PKG_VERSION}" ]; then
+       echo "Package ${GRAPHIC_CARD_PKG} has different version in chroot and host system. Maybe you need to update your host" 
+       exit 1
+    fi
+fi
+
+tar -xjf ${WORKSPACE}/unit_tests.tar.bz2 -C ${WORKSPACE}
+cd ${WORKSPACE}/${SOFTWARE}/build
+make test ARGS="-VV" || true
+
+# . /usr/share/gazebo/setup.sh
+DELIM
+
+# Make project-specific changes here
+###################################################
+
+sudo pbuilder  --execute \
+    --bindmounts $WORKSPACE \
+    --basetgz $basetgz \
+    -- build.sh
+
