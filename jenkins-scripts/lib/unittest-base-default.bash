ef3c71a5ac2b0d5d396b27593e73c89a49ed2978
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-08-16 17:43:25 +0200

    Need gazebo binaries to run test suite

--- jenkins-scripts/lib/unittest-base-default.bash
+++ jenkins-scripts/lib/unittest-base-default.bash
@@ -22,20 +22,10 @@ apt-get update
 # Required stuff for Gazebo
 apt-get install -y ${BASE_DEPENDENCIES} ${GAZEBO_BASE_DEPENDENCIES} ${GAZEBO_EXTRA_DEPENDENCIES} ${EXTRA_PACKAGES}
 
-# ! To be removed
-# Optional stuff. Check for graphic card support
-if ${GRAPHIC_CARD_FOUND}; then
-    apt-get install -y ${GRAPHIC_CARD_PKG}
-    # Check to be sure version of kernel graphic card support is the same.
-    # It will kill DRI otherwise
-    CHROOT_GRAPHIC_CARD_PKG_VERSION=\$(dpkg -l | grep "^ii.*${GRAPHIC_CARD_PKG}\ " | awk '{ print \$3 }')
-    if [ "\${CHROOT_GRAPHIC_CARD_PKG_VERSION}" != "${GRAPHIC_CARD_PKG_VERSION}" ]; then
-       echo "Package ${GRAPHIC_CARD_PKG} has different version in chroot and host system. Maybe you need to update your host" 
-       exit 1
-    fi
-fi
-
+# Install gazebo: binary version and source code to run tests from there
+tar -xvjf ${WORKSPACE}/gazebo.tar.bz2 -C /--strip 1 
 tar -xjf ${WORKSPACE}/source.tar.bz2 -C ${WORKSPACE}
+# Install the binaries of unit test suite
 tar -xjf ${WORKSPACE}/unit_tests.tar.bz2 -C ${WORKSPACE}/$SOFTWARE/build
 cd ${WORKSPACE}/${SOFTWARE}/build
 # Need to run cmake again to fix system paths
