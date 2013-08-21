c5501d137d8f9d672e43b73fd406498bfe27f55b
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-08-21 13:43:44 +0200

    New tests for precise

---
+++ jenkins-scripts/unittest-devel-default-precise-amd64.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+# Knowing Script dir beware of symlink
+[[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
+SCRIPT_DIR="${SCRIPT_DIR%/*}"
+
+
+# TODO: remove this when all unit tests are real unit test not needed GUI
+export DISPLAY=$(ps aux | grep "X :" | grep -v grep | awk '{ print $12 }')
+
+export DISTRO=precise
+export ROS_DISTRO=fuerte
+
+. ${SCRIPT_DIR}/lib/unittest-base-default.bash
