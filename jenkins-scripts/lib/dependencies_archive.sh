6689e7f54ee9b34b820d213a8f00e36471483958
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-09-27 11:03:36 -0700

    Fix fixed ros name to use proper ROS_DISTRO var

--- jenkins-scripts/lib/dependencies_archive.sh
+++ jenkins-scripts/lib/dependencies_archive.sh
@@ -18,11 +18,11 @@ if [ -z $GAZEBO_DEB_PACKAGE ]; then
 fi
 
 # image-transport-plugins is needed to properly advertise compressed image topics
-DRCSIM_BASE_DEPENDENCIES="ros-${ROS_DISTRO}-pr2-mechanism ros-${ROS_DISTRO}-std-msgs ros-${ROS_DISTRO}-common-msgs ros-${ROS_DISTRO}-image-common ros-${ROS_DISTRO}-geometry ros-${ROS_DISTRO}-pr2-controllers ros-${ROS_DISTRO}-geometry-experimental ros-${ROS_DISTRO}-robot-model-visualization ros-${ROS_DISTRO}-image-pipeline ${GAZEBO_DEB_PACKAGE} ros-${ROS_DISTRO}-image-transport-plugins ros-groovy-gazebo-plugins"
+DRCSIM_BASE_DEPENDENCIES="ros-${ROS_DISTRO}-pr2-mechanism ros-${ROS_DISTRO}-std-msgs ros-${ROS_DISTRO}-common-msgs ros-${ROS_DISTRO}-image-common ros-${ROS_DISTRO}-geometry ros-${ROS_DISTRO}-pr2-controllers ros-${ROS_DISTRO}-geometry-experimental ros-${ROS_DISTRO}-robot-model-visualization ros-${ROS_DISTRO}-image-pipeline ${GAZEBO_DEB_PACKAGE} ros-${ROS_DISTRO}-image-transport-plugins ros-${ROS_DISTRO}-gazebo-plugins"
 
 # Extra dependencies for groovy/hydro
 if [[ ${ROS_DISTRO} != 'precise' ]]; then
-     DRCSIM_BASE_DEPENDENCIES="${DRCSIM_BASE_DEPENDENCIES} ros-groovy-compressed-depth-image-transport ros-groovy-compressed-image-transport ros-groovy-theora-image-transport"
+     DRCSIM_BASE_DEPENDENCIES="${DRCSIM_BASE_DEPENDENCIES} ros-${ROS_DISTRO}-compressed-depth-image-transport ros-${ROS_DISTRO}-compressed-image-transport ros-${ROS_DISTRO}-theora-image-transport"
 fi
 
 DRCSIM_FULL_DEPENDENCIES="${DRCSIM_BASE_DEPENDENCIES} sandia-hand osrf-common"
