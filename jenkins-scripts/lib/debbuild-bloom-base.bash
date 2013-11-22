78f65b707c709579642e6d41dafe15e7c92db856
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-11-22 21:36:53 +0100

    remove more commands

--- jenkins-scripts/lib/debbuild-bloom-base.bash
+++ jenkins-scripts/lib/debbuild-bloom-base.bash
@@ -9,10 +9,6 @@ fi
 # testing jobs and seems to be slow at the end of jenkins jobs
 export ENABLE_REAPER=false
 
-PACKAGE_UNDERSCORE_NAME=${PACKAGE//-/_}
-
-. ${SCRIPT_DIR}/lib/boilerplate_prepare.sh
-
 cat > build.sh << DELIM
 ###################################################
 # Make project-specific changes here
@@ -21,21 +17,7 @@ cat > build.sh << DELIM
 set -ex
 
 # Install deb-building tools
-apt-get install -y pbuilder fakeroot debootstrap devscripts dh-make ubuntu-dev-tools mercurial git debhelper wget cdbs 
-
-# get ROS repo's key, to be used in creating the pbuilder chroot (to allow it to install packages from that repo)
-sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $DISTRO main" > /etc/apt/sources.list.d/ros-latest.list'
-wget http://packages.ros.org/ros.key -O - | apt-key add -
-# Also get drc repo's key, to be used in getting Gazebo
-sh -c 'echo "deb http://packages.osrfoundation.org/drc/ubuntu $DISTRO main" > /etc/apt/sources.list.d/drc-latest.list'
-wget http://packages.osrfoundation.org/drc.key -O - | apt-key add -
-apt-get update
-
-# Hack to avoid problem with non updated 
-if [ $DISTRO = 'precise' ]; then
-  echo "Skipping pbuilder check for outdated info"
-  #sed -i -e 's:UbuntuDistroInfo().devel():self.target_distro:g' /usr/bin/pbuilder-dist
-fi
+apt-get install -y pbuilder fakeroot debootstrap devscripts dh-make ubuntu-dev-tools 
 
 # Step 0: create/update distro-specific pbuilder environment
 pbuilder-dist $DISTRO $ARCH create --othermirror "deb http://packages.ros.org/ros/ubuntu $DISTRO main|deb http://packages.osrfoundation.org/drc/ubuntu $DISTRO main" --keyring /etc/apt/trusted.gpg --debootstrapopts --keyring=/etc/apt/trusted.gpg || true
