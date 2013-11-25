942760d2a2157d727ea2eae252a05251806251b9
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-11-25 15:08:36 +0100

    Fix sed command

--- jenkins-scripts/lib/debbuild-bloom-base.bash
+++ jenkins-scripts/lib/debbuild-bloom-base.bash
@@ -21,17 +21,12 @@ set -ex
 # Install deb-building tools
 apt-get install -y pbuilder fakeroot debootstrap devscripts dh-make ubuntu-dev-tools wget
 
-wget http://packages.ros.org/ros.key -O - | apt-key add -
+#wget http://packages.ros.org/ros.key -O - | apt-key add -
 
 sed -i -e 's:-q::g' /usr/lib/pbuilder/pbuilder-createbuildenv  
 
 # Step 0: create/update distro-specific pbuilder environment
 pbuilder-dist $DISTRO $ARCH create --othermirror "deb http://packages.ros.org/ros/ubuntu $DISTRO main|deb http://packages.osrfoundation.org/drc/ubuntu $DISTRO main" --keyring /etc/apt/trusted.gpg --debootstrapopts || true
-
-mkdir -p $WORKSPACE/pkgs
-
-find . -name core || true
-find . -name core -exec cp {} $WORKSPACE/pkgs 
 DELIM
 
 #
