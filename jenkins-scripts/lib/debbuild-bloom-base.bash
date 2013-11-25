e8c8e95476850cefa8a2199063a6e1ec0a30ae07
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-11-25 16:40:15 +0100

    Do not repeat keyring

--- jenkins-scripts/lib/debbuild-bloom-base.bash
+++ jenkins-scripts/lib/debbuild-bloom-base.bash
@@ -26,7 +26,7 @@ wget http://packages.ros.org/ros.key -O - | apt-key add -
 sed -i -e 's:-q::g' /usr/lib/pbuilder/pbuilder-createbuildenv  
 
 # Step 0: create/update distro-specific pbuilder environment
-pbuilder-dist $DISTRO $ARCH create --othermirror "deb http://packages.ros.org/ros/ubuntu $DISTRO main|deb http://packages.osrfoundation.org/drc/ubuntu $DISTRO main" --keyring /etc/apt/trusted.gpg --debootstrapopts --keyring=/etc/apt/trusted.gpg
+pbuilder-dist $DISTRO $ARCH create --othermirror "deb http://packages.ros.org/ros/ubuntu $DISTRO main|deb http://packages.osrfoundation.org/drc/ubuntu $DISTRO main" --keyring /etc/apt/trusted.gpg --debootstrapopts
 DELIM
 
 #
