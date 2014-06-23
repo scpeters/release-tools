a2fa225ed08f092c20f24a81517e18e8d527633f
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-06-23 19:01:32 +0200

    Be able to built only HEAD projects

--- jenkins-scripts/lib/project-default-devel-homebrew-amd64.bash
+++ jenkins-scripts/lib/project-default-devel-homebrew-amd64.bash
@@ -38,8 +38,8 @@ done || true
 # Process the package dependencies
 # Run twice! details about why in:
 # https://github.com/osrf/homebrew-simulation/pull/18#issuecomment-45041755 
-${RUN_DIR}/bin/brew install ${PROJECT} ${PROJECT_ARGS} --only-dependencies
-${RUN_DIR}/bin/brew install ${PROJECT} ${PROJECT_ARGS} --only-dependencies
+${RUN_DIR}/bin/brew install --HEAD ${PROJECT} ${PROJECT_ARGS} --only-dependencies
+${RUN_DIR}/bin/brew install --HEAD ${PROJECT} ${PROJECT_ARGS} --only-dependencies
 
 # Step 3. Manually compile and install ${PROJECT}
 cd ${WORKSPACE}/${PROJECT}
