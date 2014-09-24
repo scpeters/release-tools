b42c0e6c54a69ccacf00a0645cf6313b5d9f9136
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-09-24 20:57:41 +0200

    First implementation of docker instructions

--- jenkins-scripts/lib/boilerplate_prepare.sh
+++ jenkins-scripts/lib/boilerplate_prepare.sh
@@ -7,7 +7,7 @@ if [ -z ${DISTRO} ]; then
 fi
 
 if [ -z ${ROS_DISTRO} ]; then
-  ROS_DISTRO=groovy
+  ROS_DISTRO=hydro
 fi
 
 # Define making jobs by default if not present
@@ -53,32 +53,10 @@ fi
 . ${SCRIPT_DIR}/lib/check_graphic_card.bash
 . ${SCRIPT_DIR}/lib/dependencies_archive.sh
 
-# Workaround for precise pbuilder-dist segfault
-# https://bitbucket.org/osrf/release-tools/issue/22
-if [[ -z $WORKAROUND_PBUILDER_BUG ]]; then
-  WORKAROUND_PBUILDER_BUG=false
-fi
-
-if $WORKAROUND_PBUILDER_BUG && [[ $DISTRO == 'precise' ]]; then
-  distro=trusty
-else
-  distro=${DISTRO}
-fi
-
-arch=amd64
-base=/var/cache/pbuilder-$distro-$arch
-aptconffile=$WORKSPACE/apt.conf
-
-#increment this value if you have changed something that will invalidate base tarballs. #TODO this will need cleanup eventually.
-basetgz_version=2
-
-rootdir=$base/apt-conf-$basetgz_version
-
-basetgz=$base/base-$basetgz_version.tgz
 output_dir=$WORKSPACE/output
 work_dir=$WORKSPACE/work
 
-NEEDED_HOST_PACKAGES="mercurial pbuilder python-empy debhelper python-setuptools python-psutil"
+NEEDED_HOST_PACKAGES="mercurial docker.io python-setuptools python-psutil"
 # python-argparse is integrated in libpython2.7-stdlib since raring
 # Check for precise in the HOST system (not valid DISTRO variable)
 if [[ $(lsb_release -sr | cut -c 1-5) == '12.04' ]]; then
@@ -94,68 +72,6 @@ if [[ -n ${QUERY_HOST_PACKAGES} ]]; then
   sudo apt-get install -y ${NEEDED_HOST_PACKAGES}
 fi
 
-# monitor all subprocess and enforce termination (thanks to ROS crew)
-# never failed on this
-if $ENABLE_REAPER; then
-# Hack for not failing when github is down
-download_done=false
-seconds_waiting=0
-while (! $download_done); do
-  wget https://raw.github.com/ros-infrastructure/buildfarm/master/scripts/subprocess_reaper.py -O subprocess_reaper.py && download_done=true
-  sleep 1
-  seconds_waiting=$((seconds_waiting+1))
-  [ $seconds_waiting -gt 60 ] && exit 1
-done
-
-sudo python subprocess_reaper.py $$ &
-sleep 1
-fi
-
-#setup the cross platform apt environment
-# using sudo since this is shared with pbuilder and if pbuilder is interupted it will leave a sudo only lock file.  Otherwise sudo is not necessary. 
-# And you can't chown it even with sudo and recursive
-cd $WORKSPACE/scripts/catkin-debs/
-
-ubuntu_repo_url="http://us.archive.ubuntu.com/ubuntu"
-
-# If using a depracted distro, you need to use old-releases from ubuntu
-if [[ $DISTRO == 'raring' ]]; then
-  ubuntu_repo_url="http://old-releases.ubuntu.com/ubuntu/"
-fi
-
-if $ENABLE_ROS; then
-  ros_repository_str="--repo ros@http://packages.ros.org/ros/ubuntu"
-fi
-
-sudo ./setup_apt_root.py $distro $arch $rootdir \
-                          --mirror $ubuntu_repo_url $ros_repository_str \
-			  --local-conf-dir $WORKSPACE 
-sudo rm -rf $output_dir
-mkdir -p $output_dir
-
-sudo rm -rf $work_dir
-mkdir -p $work_dir
-cd $work_dir
-
-sudo apt-get update -c $aptconffile
-
-# Check if trusty exists in the machine (not in precise) and symlink
-if [[ ! -f /usr/share/debootstrap/scripts/trusty ]]; then
-    sudo ln -s /usr/share/debootstrap/scripts/gutsy /usr/share/debootstrap/scripts/trusty
-fi
-
-# Setup the pbuilder environment if not existing, or update
-if [ ! -e $basetgz ] || [ ! -s $basetgz ] 
-then
-  #make sure the base dir exists
-  sudo mkdir -p $base
-  #create the base image
-  sudo pbuilder create \
-    --distribution $distro \
-    --aptconfdir $rootdir/etc/apt \
-    --basetgz $basetgz \
-    --architecture $arch \
-    --mirror $ubuntu_repo_url
-else
-  sudo pbuilder --update --basetgz $basetgz --mirror $ubuntu_repo_url
-fi
+rm -fr Dockerfile
+cd ${WORKSPACE}
+sudo docker pull ubuntu:${DISTRO}
--- jenkins-scripts/lib/sdformat-default.bash
+++ jenkins-scripts/lib/sdformat-default.bash
@@ -9,7 +9,12 @@ cat > build.sh << DELIM
 #
 set -ex
 
-# Step 1: install everything you need
+# Step 1: Configure apt
+echo "deb http://archive.ubuntu.com/ubuntu ${DISTRO} main restricted universe multiverse" > /etc/apt/sources.list
+echo "deb http://archive.ubuntu.com/ubuntu ${DISTRO}-updates main restricted universe multiverse" > /etc/apt/sources.list
+echo "deb http://archive.ubuntu.com/ubuntu ${DISTRO}-security main restricted universe multiverse" > /etc/apt/sources.list
+
+apt-get update
 apt-get install -y ${BASE_DEPENDENCIES} ${SDFORMAT_BASE_DEPENDENCIES}
 
 # Step 2: configure and build
@@ -27,10 +32,31 @@ sh tools/code_check.sh -xmldir $WORKSPACE/build/cppcheck_results || true
 cat $WORKSPACE/build/cppcheck_results/*.xml
 DELIM
 
-# Make project-specific changes here
-###################################################
+cat > Dockerfile << DELIM_DOCKER
+#######################################################
+# Docker file to run build.sh
 
-sudo pbuilder  --execute \
-    --bindmounts $WORKSPACE \
-    --basetgz $basetgz \
-    -- build.sh
+FROM ubuntu:${DISTRO}
+MAINTAINER Jose Luis Rivero <jrivero@osrfoundation.org>
+
+# If host is running squid-deb-proxy on port 8000, populate /etc/apt/apt.conf.d/30proxy
+# By default, squid-deb-proxy 403s unknown sources, so apt shouldn't proxy ppa.launchpad.net
+RUN route -n | awk '/^0.0.0.0/ {print \$2}' > /tmp/host_ip.txt
+RUN echo "HEAD /" | nc \$(cat /tmp/host_ip.txt) 8000 | grep squid-deb-proxy \
+  && (echo "Acquire::http::Proxy \"http://\$(cat /tmp/host_ip.txt):8000\";" > /etc/apt/apt.conf.d/30proxy) \
+  && (echo "Acquire::http::Proxy::ppa.launchpad.net DIRECT;" >> /etc/apt/apt.conf.d/30proxy) \
+  || echo "No squid-deb-proxy detected on docker host"
+
+# Map the workspace into the container
+RUN mkdir -p ${WORKSPACE}
+ADD sdformat ${WORKSPACE}/sdformat
+ADD build.sh build.sh
+RUN chmod +x build.sh
+RUN ./build.sh
+DELIM_DOCKER
+
+sudo docker build -t sdformat/dev .
+CID=$(sudo docker run -d -t sdformat/dev /bin/bash)
+sudo docker cp ${CID}:${WORKSPACE}/build/test_results     ${WORKSPACE}/build
+sudo docker cp ${CID}:${WORKSPACE}/build/cppcheck_results ${WORKSPACE}/build
+sudo docker stop ${CID}
