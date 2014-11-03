1b143bc7309dfdb72f909f0ecdc1d2cbfbe0b063
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-11-03 15:13:31 -0800

    debug command

--- jenkins-scripts/debbuild_in_debian.bash
+++ jenkins-scripts/debbuild_in_debian.bash
@@ -75,7 +75,8 @@ sed -i -e "s/ddddd/\${RELEASE_DATE}/g" debian/changelog
 
 # Get into the unpacked source directory, without explicit knowledge of that 
 # directory's name
-cd \`find $WORKSPACE/build -mindepth 1 -type d |head -n 1\`
+ls $WORKSPACE/build
+find $WORKSPACE/build -mindepth 1 -type d
 # If use the quilt 3.0 format for debian (drcsim) it needs a tar.gz with sources
 if $NIGHTLY_MODE; then
   rm -fr .hg*
