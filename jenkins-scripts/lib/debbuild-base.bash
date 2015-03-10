64f7d984574ccaa3be7f646ca61f89300dd9d076
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-03-10 02:56:34 +0100

    Reenable squid and proper dir structure

--- jenkins-scripts/lib/debbuild-base.bash
+++ jenkins-scripts/lib/debbuild-base.bash
@@ -211,15 +211,13 @@ cat > Dockerfile << DELIM_DOCKER
 FROM osrf/ubuntu_armhf:${DISTRO}
 MAINTAINER Jose Luis Rivero <jrivero@osrfoundation.org>
 
-Disable by now
-
 # If host is running squid-deb-proxy on port 8000, populate /etc/apt/apt.conf.d/30proxy
 # By default, squid-deb-proxy 403s unknown sources, so apt shouldn't proxy ppa.launchpad.net
-# RUN route -n | awk '/^0.0.0.0/ {print \$2}' > /tmp/host_ip.txt
-# RUN echo "HEAD /" | nc \$(cat /tmp/host_ip.txt) 8000 | grep squid-deb-proxy \
-#  && (echo "Acquire::http::Proxy \"http://\$(cat /tmp/host_ip.txt):8000\";" > /etc/apt/apt.conf.d/30proxy) \
-#  && (echo "Acquire::http::Proxy::ppa.launchpad.net DIRECT;" >> /etc/apt/apt.conf.d/30proxy) \
-#  || echo "No squid-deb-proxy detected on docker host"
+RUN route -n | awk '/^0.0.0.0/ {print \$2}' > /tmp/host_ip.txt
+RUN echo "HEAD /" | nc \$(cat /tmp/host_ip.txt) 8000 | grep squid-deb-proxy \
+  && (echo "Acquire::http::Proxy \"http://\$(cat /tmp/host_ip.txt):8000\";" > /etc/apt/apt.conf.d/30proxy) \
+  && (echo "Acquire::http::Proxy::ppa.launchpad.net DIRECT;" >> /etc/apt/apt.conf.d/30proxy) \
+  || echo "No squid-deb-proxy detected on docker host"
 
 # Map the workspace into the container
 RUN mkdir -p /var/packages/gazebo/ubuntu
