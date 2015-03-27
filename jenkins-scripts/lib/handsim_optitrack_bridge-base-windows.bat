34183034856d46237fb1b9d5e331f1caee3e83ed
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-03-27 01:46:47 +0100

    Improve optitrack script!

--- jenkins-scripts/lib/handsim_optitrack_bridge-base-windows.bat
+++ jenkins-scripts/lib/handsim_optitrack_bridge-base-windows.bat
@@ -7,34 +7,56 @@
 set win_lib=%SCRIPT_DIR%\lib\windows_library.bat
 
 :: Call vcvarsall and all the friends
+echo # BEGIN SECTION: configure the MSVC compiler
 call %win_lib% :configure_msvc_compiler
+echo # END SECTION
 
+echo # BEGIN SECTION: setup workspace
 cd %WORKSPACE%
-IF exist workspace ( rmdir /s /q workspace ) || goto %win_lib% :error
+IF exist workspace ( rmdir /s /q workspace ) || goto :error
 mkdir workspace
-move handsim %WORKSPACE%/workspace/handsim || goto %win_lib% :error
+cd workspace
+echo # END SECTION
 
-echo "Downloading optitrack libraries ..."
+echo # BEGIN SECTION: move sources so we agree with configure.bat layout
+set HANDSIM_WS=%WORKSPACE%\workspace\handsim
+if exist %HANDSIM_WS% ( rm /s /q %HANDSIM_WS% || goto :error )
+xcopy %WORKSPACE%\handsim %HANDSIM_WS% /s /i /e > xcopy.log || goto :error
+echo # END SECTION
+
+
+echo # BEGIN SECTION: downloading optitrack libraries
 cd %WORKSPACE%/workspace
 mkdir bridgeLibs
 cd bridgeLibs
 call %win_lib% :download_7za
 call %win_lib% :wget 'https://www.dropbox.com/s/tkc25e1pzn4lm8f/bridgeLibs.zip?dl=1' bridgeLibs.zip
 call %win_lib% :unzip_7za bridgeLibs.zip
+echo # END SECTION
 
-echo "Compiling handsim/windows ..."
-cd %WORKSPACE%/workspace/handsim/windows || goto %win_lib% :error
+echo # BEGIN SECTION: compiling handsim/windows
+cd %WORKSPACE%/workspace/handsim/windows || goto :error
 mkdir build
 cd build
 copy %WORKSPACE%\workspace\NPTrackingToolsx64.dll .
-call "..\configure.bat" Release %BITNESS% || goto %win_lib% :error
-nmake || goto %win_lib% :error
+call "..\configure.bat" Release %BITNESS% || goto :error
+nmake || goto :error
+echo # END SECTION
 
-echo "Generating the zip"
+echo # BEGIN SECTION: generating the zip
 mkdir %WORKSPACE%\workspace\package || goto %win_lib% :error
 cd %WORKSPACE%\workspace\package || goto %win_lib% :error
 copy %WORKSPACE%\workspace\bridgeLibs\* .
 copy %WORKSPACE%\workspace\handsim\windows\build\*.exe .
 
 cd %WORKSPACE%\workspace\
-"%WORKSPACE%\workspace\7za.exe" a -tzip optitrack_bridge.zip %WORKSPACE%\workspace\package
+call %win_lib% :download_7za
+7za.exe a -tzip optitrack_bridge.zip %WORKSPACE%\workspace\package
+echo # END SECTION
+
+goto :EOF
+
+:error - error routine
+::
+echo Failed with error #%errorlevel%.
+exit /b %errorlevel%
