2c7d932a465a80a438cb40234b6f0a7d3e76cdff
Steve Peters <scpeters@osrfoundation.org>
2015-04-24 14:54:58 -0700

    test with ign-math2 branch

--- jenkins-scripts/lib/sdformat-base-windows.bat
+++ jenkins-scripts/lib/sdformat-base-windows.bat
@@ -23,6 +23,7 @@ IF %USE_IGNITION_ZIP% == FALSE (
   echo # BEGIN SECTION: compile and install ign-math
   IF exist %WORKSPACE%\ign-math ( rmdir /s /q %WORKSPACE%\ign-math ) || goto :error
   hg clone https://bitbucket.org/ignitionrobotics/ign-math %WORKSPACE%\ign-math || goto :error
+  hg up ign-math2
   set VCS_DIRECTORY=ign-math
   set KEEP_WORKSPACE=TRUE
   call "%SCRIPT_DIR%\lib\project-default-devel-windows.bat"
