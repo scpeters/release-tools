eb8bec2fb609666c889658fcc70ffcb51ce6af66
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-05-13 19:09:24 +0200

    Another workaround

--- jenkins-scripts/docker/lib/handsim-check-release.bash
+++ jenkins-scripts/docker/lib/handsim-check-release.bash
@@ -42,13 +42,12 @@ echo '# BEGIN SECTION: test arat.world'
 # Seems like there is no failure in runs on precise pbuilder in
 # our trusty machine. So we do not check for GRAPHIC_TESTS=true
 mkdir -p \$HOME/.gazebo
-# Set the locale - see http://jaredmarkell.com/docker-and-locales/
-# a work around for this error when running tilemill:
+# Workaround for bug:
 # what():  locale::facet::_S_create_c_locale name not valid
-locale-gen en_US.UTF-8  
-export LANG=en_US.UTF-8  
-export LANGUAGE=en_US:en  
-export LC_ALL=en_US.UTF-8 
+export DEBIAN_FRONTEND=noninteractive
+apt-get install locales
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+locale-gen
 timeout 180 gazebo worlds/arat.world || echo "Failure response in the launch command" && exit 1
 echo "180 testing seconds finished successfully"
 echo '# END SECTION'
