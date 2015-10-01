f58cbd904ed8d1b595139a15534fce019e290918
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-10-01 17:19:06 +0200

    New multiany script

---
+++ jenkins-scripts/docker/gazebo-default-dart_multiany-trusty-amd64.bash
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+
+# Knowing Script dir beware of symlink
+[[ -L ${0} ]] && SCRIPT_DIR=$(readlink ${0}) || SCRIPT_DIR=${0}
+SCRIPT_DIR="${SCRIPT_DIR%/*}"
+
+export GPU_SUPPORT_NEEDED=true
+export DISTRO=trusty
+
+# By default true
+export DART_FROM_PKGS=${DART_FROM_PKGS:=true}
+export DART_COMPILE_FROM_SOURCE=${DART_COMPILE_FROM_SOURCE:=false}
+export DART_COMPILE_FROM_SOURCE_BRANCH=${DART_COMPILE_FROM_SOURCE:=default}
+
+. ${SCRIPT_DIR}/lib/gazebo-base-default.bash
