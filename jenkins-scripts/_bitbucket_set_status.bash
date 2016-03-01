a384a2099bcee8ccea83900a8ee2ea5262a3ff80
Jose Luis Rivero <jrivero@osrfoundation.org>
2016-03-01 23:37:02 +0100

    Fix problem with path

--- jenkins-scripts/_bitbucket_set_status.bash
+++ jenkins-scripts/_bitbucket_set_status.bash
@@ -21,13 +21,12 @@ if [[ ! -f $BITBUCKET_BUILD_STATUS_FILE ]]; then
 fi
 
 if [[ ! -f ${BITBUCKET_USER_PASS_FILE} ]]; then
-  echo "Bitbucket user pass not found in file \${BITBUCKET_USER_PASS_FILE}"
+  echo "Bitbucket user pass not found in file: ${BITBUCKET_USER_PASS_FILE}"
   exit 1
 fi
 
 echo "BEGIN SECTION: build status in bitbucket: ${STATUS} (hidden)"
 set +x # keep password secret
-BITBUCKET_USER_PASS=`cat $BITBUCKET_USER_PASS_FILE`
 ${WORKSPACE}/scripts/jenkins-scripts/python-bitbucket/set_status_from_file.py \
     --user osrf_jenkins  \
     --pass ${BITBUCKET_USER_PASS} \
