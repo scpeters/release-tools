d59b8a8d5fe4791a2274e5d7a01081ac6bc5c706
Jose Luis Rivero <jrivero@osrfoundation.org>
2016-03-28 19:45:15 +0200

    Temporal hack to run pre-compiler headers and get timming

--- jenkins-scripts/docker/lib/gazebo-base-default.bash
+++ jenkins-scripts/docker/lib/gazebo-base-default.bash
@@ -21,33 +21,15 @@ if [[ ${GAZEBO_BASE_CMAKE_ARGS} != ${GAZEBO_BASE_CMAKE_ARGS/Coverage} ]]; then
   EXTRA_PACKAGES="${EXTRA_PACKAGES} lcov" 
 fi
 
-cat > build.sh << DELIM
+for i in [1..2]; do
+cat >> build.sh << DELIM
 ###################################################
 # Make project-specific changes here
 #
 set -ex
+source ${TIMING_DIR}/_time_lib.sh ${WORKSPACE}
 
-# Step 2: configure and build
-# Check for DART
-if $DART_COMPILE_FROM_SOURCE; then
-  echo '# BEGIN SECTION: compiling DART from source'
-  if [ -d $WORKSPACE/dart ]; then
-      cd $WORKSPACE/dart
-      git pull
-  else
-     git clone https://github.com/dartsim/dart.git $WORKSPACE/dart
-  fi
-  rm -fr $WORKSPACE/dart/build
-  mkdir -p $WORKSPACE/dart/build
-  cd $WORKSPACE/dart/build
-  cmake .. \
-      -DCMAKE_INSTALL_PREFIX=/usr
-  #make -j${MAKE_JOBS}
-  make -j1
-  make install
-  echo '# END SECTION'
-fi
-
+init_stopwatch COMPILATION_${i}
 # Normal cmake routine for Gazebo
 echo '# BEGIN SECTION: Gazebo configuration'
 rm -rf $WORKSPACE/install
@@ -62,49 +44,9 @@ echo '# END SECTION'
 echo '# BEGIN SECTION: Gazebo compilation'
 make -j${MAKE_JOBS}
 echo '# END SECTION'
-
-echo '# BEGIN SECTION: Gazebo installation'
-make install
-. /usr/share/gazebo/setup.sh
-echo '# END SECTION'
-
-# Need to clean up from previous built
-rm -fr $WORKSPACE/cppcheck_results
-rm -fr $WORKSPACE/test_results
-
-# Run tests
-if [ `expr length "${GAZEBO_BASE_TESTS_HOOK} "` -gt 1 ]; then
-  ${GAZEBO_BASE_TESTS_HOOK}
-  : # keep this line, needed if the variable is empty
-else
-  # Run default
-  echo '# BEGIN SECTION: UNIT testing'
-  make test ARGS="-VV -R UNIT_*" || true
-  echo '# END SECTION'
-  echo '# BEGIN SECTION: INTEGRATION testing'
-  make test ARGS="-VV -R INTEGRATION_*" || true
-  echo '# END SECTION'
-  echo '# BEGIN SECTION: REGRESSION testing'
-  make test ARGS="-VV -R REGRESSION_*" || true
-  echo '# END SECTION'
-  echo '# BEGIN SECTION: EXAMPLE testing'
-  make test ARGS="-VV -R EXAMPLE_*" || true
-  echo '# END SECTION'
-fi
-
-# Only run cppcheck on trusty
-if [ "$DISTRO" = "trusty" ] || [ "$DISTRO" = "wily" ]; then
-  echo '# BEGIN SECTION: running cppcheck'
-  # Step 3: code check
-  cd $WORKSPACE/gazebo
-  sh tools/code_check.sh -xmldir $WORKSPACE/build/cppcheck_results || true
-  echo '# END SECTION'
-else
-  mkdir -p $WORKSPACE/build/cppcheck_results/
-  echo "<results></results>" >> $WORKSPACE/build/cppcheck_results/empty.xml 
-fi
-echo '# END SECTION'
+init_stopwatch COMPILATION_${i}
 DELIM
+done
 
 USE_OSRF_REPO=true
 SOFTWARE_DIR="gazebo"
