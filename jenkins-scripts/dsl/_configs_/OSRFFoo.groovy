47902b38cc01c495d570de80c7e3f850cee946ca
Jose Luis Rivero <jrivero@osrfoundation.org>
2016-04-01 15:07:43 +0200

    Back to root

--- jenkins-scripts/dsl/_configs_/OSRFFoo.groovy
+++ jenkins-scripts/dsl/_configs_/OSRFFoo.groovy
@@ -2,31 +2,34 @@ package _configs_
 
 import javaposse.jobdsl.dsl.Job
 
-def create_status_name = Globals.bitbucket_build_status_job_name
+/*
+  -> GenericMail
 
-/* 
- * -> OSRFWorkFlow Main
- *
- *  Implements:
- *    - label
- *    - workflow definition
- *    - parameters
- */
+  Implements:
+     - description
+     - RTOOLS parame + groovy to set jobDescription
+     - base mail for Failures and Unstables
+*/
+
 class OSRFFoo
 {
    static void create(Job job, String build_any_job_name)
    {
+     GenericMail.include_mail(job)
+
      job.with
      {
-       label "master || docker"
+     	description 'Automatic generated job by DSL jenkins. Please do not edit manually'
 
-      parameters {
-        stringParam('RTOOLS_BRANCH','default','release-tools branch to send to jobs')
-        stringParam('SRC_REPO','','URL pointing to repository')
-        stringParam('SRC_BRANCH','default','Branch of SRC_REPO to test')
-        stringParam('JOB_DESCRIPTION','','Description of the job in course. For information proposes.')
-        stringParam('DEST_BRANCH','default','Branch to merge in')
-      } // end of parameters
-    } // end of job
-  } // end of create
-} 
+        parameters {
+          stringParam('RTOOLS_BRANCH','default','release-tool branch to use')
+          booleanParam('NO_MAILS',false,'do not send any notification by mail')
+        }
+
+        steps
+        {
+           systemGroovyCommand("build.setDescription('RTOOLS_BRANCH: ' + build.buildVariableResolver.resolve('RTOOLS_BRANCH'));")
+        }
+      }
+    }
+}
