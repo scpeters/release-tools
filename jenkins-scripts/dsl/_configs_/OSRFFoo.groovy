05ee7b56c80eacf4098f5554e3beeefab4840dfe
Jose Luis Rivero <jrivero@osrfoundation.org>
2016-04-01 15:09:01 +0200

    Simple version

--- jenkins-scripts/dsl/_configs_/OSRFFoo.groovy
+++ jenkins-scripts/dsl/_configs_/OSRFFoo.groovy
@@ -15,21 +15,9 @@ class OSRFFoo
 {
    static void create(Job job, String build_any_job_name)
    {
-     GenericMail.include_mail(job)
-
-     job.with
-     {
-     	description 'Automatic generated job by DSL jenkins. Please do not edit manually'
-
-        parameters {
-          stringParam('RTOOLS_BRANCH','default','release-tool branch to use')
-          booleanParam('NO_MAILS',false,'do not send any notification by mail')
-        }
-
-        steps
-        {
-           systemGroovyCommand("build.setDescription('RTOOLS_BRANCH: ' + build.buildVariableResolver.resolve('RTOOLS_BRANCH'));")
-        }
+      job.with
+      {
+        label "master || docker"
       }
-    }
+   }
 }
