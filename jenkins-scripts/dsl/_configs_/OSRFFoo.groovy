6833bf07b5f71f3a985dce0b2fe21955b708d332
Jose Luis Rivero <jrivero@osrfoundation.org>
2016-04-01 15:13:03 +0200

    Added parameters

--- jenkins-scripts/dsl/_configs_/OSRFFoo.groovy
+++ jenkins-scripts/dsl/_configs_/OSRFFoo.groovy
@@ -19,6 +19,15 @@ class OSRFFoo
       {
         label "master || docker"
 
+        // TODO: share parameters with ci-py_any- jobs
+        parameters {
+          stringParam('RTOOLS_BRANCH','default','release-tools branch to send to jobs')
+          stringParam('SRC_REPO','','URL pointing to repository')
+          stringParam('SRC_BRANCH','default','Branch of SRC_REPO to test')
+          stringParam('JOB_DESCRIPTION','','Description of the job in course. For information proposes.')
+          stringParam('DEST_BRANCH','default','Branch to merge in')
+        }
+
         definition
         {
           cps
