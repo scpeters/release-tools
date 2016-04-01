53d939352336f0e9ee6758fa4f45aa51d9a0dc4f
Jose Luis Rivero <jrivero@osrfoundation.org>
2016-04-01 15:03:57 +0200

    Reduce the script

--- jenkins-scripts/dsl/_configs_/OSRFFoo.groovy
+++ jenkins-scripts/dsl/_configs_/OSRFFoo.groovy
@@ -24,9 +24,9 @@ class OSRFFoo
        {
          cps
          {
-          // run script in sandbox groovy
-          sandbox()
-          script("""\
+           // run script in sandbox groovy
+           sandbox()
+           script("""\
                  currentBuild.description =  "\$JOB_DESCRIPTION"
                  def archive_number = ""
 
@@ -38,59 +38,9 @@ class OSRFFoo
                     sh 'echo `hg id -i` > SCM_hash'
                     env.MERCURIAL_REVISION_SHORT = readFile('SCM_hash').trim()
                   }
-
-                 stage 'create bitbucket status file'
-                  node {
-                    def bitbucket_metadata = build job: '${create_status_name}',
-                          propagate: false, wait: true,
-                          parameters:
-                            [[\$class: 'StringParameterValue', name: 'RTOOLS_BRANCH',          value: "\$RTOOLS_BRANCH"],
-                             [\$class: 'StringParameterValue', name: 'JENKINS_BUILD_REPO',     value: "\$SRC_REPO"],
-                             [\$class: 'StringParameterValue', name: 'JENKINS_BUILD_HG_HASH',  value: env.MERCURIAL_REVISION_SHORT],
-                             [\$class: 'StringParameterValue', name: 'JENKINS_BUILD_JOB_NAME', value: env.JOB_NAME],
-                             [\$class: 'StringParameterValue', name: 'JENKINS_BUILD_URL',      value: env.BUILD_URL]]
-                    archive_number = bitbucket_metadata.getNumber().toString()
-                  }
-
-                  stage 'set bitbucket status: in progress'
-                  node {
-                     build job: '_bitbucket-set_status',
-                       parameters:
-                          [[\$class: 'StringParameterValue', name: 'RTOOLS_BRANCH',           value: "\$RTOOLS_BRANCH"],
-                           [\$class: 'StringParameterValue', name: 'BITBUCKET_STATUS',        value: "inprogress"],
-                           [\$class: 'StringParameterValue', name: 'CREATE_CONFIG_BUILD_NUM', value: archive_number]]
-                  }
-
-                 stage 'compiling + QA'
-                 node {
-                  def compilation = build job: ${build_any_job_name},
-                        propagate: true, wait: true,
-                        parameters:
-                         [[\$class: 'StringParameterValue',  name: 'RTOOLS_BRANCH',   value: "\$RTOOLS_BRANCH"],
-                          [\$class: 'BooleanParameterValue', name: 'NO_MAILS',        value: false],
-                          [\$class: 'StringParameterValue',  name: 'SRC_REPO',        value: "\$SRC_REPO"],
-                          [\$class: 'StringParameterValue',  name: 'SRC_BRANCH',      value: "\$SRC_BRANCH"],
-                          [\$class: 'StringParameterValue',  name: 'JOB_DESCRIPTION', value: "\$JOB_DESCRIPTION"],
-                          [\$class: 'StringParameterValue',  name: 'DEST_BRANCH',     value: "\$DEST_BRANCH"]]
-                }
-
-                publish_result = 'failed'
-                if (compilation.getResult() == 'SUCCESS')
-                {
-                  publish_result = 'ok'
-                }
-
-                stage 'publish bitbucket status'
-                node {
-                 build job: '_bitbucket-set_status',
-                   parameters:
-                      [[\$class: 'StringParameterValue', name: 'RTOOLS_BRANCH',           value: "\$RTOOLS_BRANCH"],
-                       [\$class: 'StringParameterValue', name: 'STATUS',                  value: publish_result ],
-                       [\$class: 'StringParameterValue', name: 'CREATE_CONFIG_BUILD_NUM', value: archive_number]]
-                }
               """.stripIndent())
-         }
-       }
+        }
+      }
  
       parameters {
         stringParam('RTOOLS_BRANCH','default','release-tools branch to send to jobs')
