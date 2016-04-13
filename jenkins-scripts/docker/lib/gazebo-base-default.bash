07155ae80bcd13495f7a2d38c3049f6b4fce5bd3
Jose Luis Rivero <jrivero@osrfoundation.org>
2016-04-13 02:14:58 +0200

    Fix pkg name

--- jenkins-scripts/docker/lib/gazebo-base-default.bash
+++ jenkins-scripts/docker/lib/gazebo-base-default.bash
@@ -31,7 +31,7 @@ set -ex
 echo '# BEGIN SECTION: install urdfdom 0.4 from PPA'
 apt-get install -y software-properties-common
 apt-add-repository -y ppa:j-rivero/urdfdom0.4-xenial
-apt-get install -y urdfdom libsdformat4-dev
+apt-get install -y liburdfdom-dev libsdformat4-dev
 echo '# END SECTION"
 
 # Step 2: configure and build
