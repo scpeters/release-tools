c5b9d8f60ab445a43a2c0da27f05a220c06bec85
Jose Luis Rivero <jrivero@osrfoundation.org>
2016-04-18 16:10:33 +0200

    Missing file

---
+++ jenkins-scripts/dsl/_configs_/OSRFLinuxBuildPkg.groovy
@@ -0,0 +1,47 @@
+package _configs_
+
+import javaposse.jobdsl.dsl.Job
+
+/*
+  -> OSRFLinuxBuilBasePkg
+
+  Implements:
+    - parameters:
+        - SOURCE_TARBALL_URI
+        - RELEASE_REPO_BRANCH
+        - UPLOAD_TO_REPO
+    - publish artifacts
+    - launch repository_ng
+*/
+class OSRFLinuxBuildPkg
+{  
+  static void create(Job job)
+  {
+    OSRFLinuxBuildBasePkg.create(job)
+
+    job.with
+    {
+      parameters {
+        stringParam("SOURCE_TARBALL_URI", null, "URL to the tarball containing the package sources")
+        stringParam("RELEASE_REPO_BRANCH", null, "Branch from the -release repo to be used")
+        stringParam("UPLOAD_TO_REPO", null, "OSRF repo name to upload the package to")
+      }
+
+      steps {
+        systemGroovyCommand("""\
+          build.setDescription(
+          '<b>' + build.buildVariableResolver.resolve('VERSION') + '-' + 
+          build.buildVariableResolver.resolve('RELEASE_VERSION') + '</b>' +
+          '(' + build.buildVariableResolver.resolve('DISTRO') + '/' + 
+                build.buildVariableResolver.resolve('ARCH') + ')' +
+          '<br />' +
+          'branch: ' + build.buildVariableResolver.resolve('RELEASE_REPO_BRANCH') + ' | ' +
+          'upload to: ' + build.buildVariableResolver.resolve('UPLOAD_TO_REPO') +
+          '<br />' +
+          'RTOOLS_BRANCH: ' + build.buildVariableResolver.resolve('RTOOLS_BRANCH'));
+          """.stripIndent()
+        )
+      }
+    } // end of job
+  } // end of method createJob
+} // end of class
