6e2092bae1fd8f2d58868e1fb6eb9b8843441512
Jose Luis Rivero <jrivero@osrfoundation.org>
2016-07-22 14:54:55 +0200

    Use eval (ugh) instead of envsubt to keep current code working

--- jenkins-scripts/docker/lib/_generic_linux_compilation_build.sh.bash
+++ jenkins-scripts/docker/lib/_generic_linux_compilation_build.sh.bash
@@ -3,6 +3,10 @@
 # independent (to be run) in its own file and having this to generate a
 # build.sh (not run at the moment) with the compilation code (vars expanded)
 
+# Using eval not to break current code that define variables without export
+# so envsubst can not be used since it does not expand script defined vars
+BUILD_SH=$(eval $(cat ${SCRIPT_DIR}/lib/_generic_linux_compilation.bash))
+
 cat > build.sh <<- EOF
-$(envsubst < ${SCRIPT_DIR}/lib/_generic_linux_compilation.bash)
+${BUILD_SH}
 EOF
