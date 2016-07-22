1709e9678a3178b1966f00ba876398e6c0383229
Jose Luis Rivero <jrivero@osrfoundation.org>
2016-07-22 14:34:13 +0200

    Fix variable expansion inside build.sh

--- jenkins-scripts/docker/lib/_generic_linux_compilation_build.sh.bash
+++ jenkins-scripts/docker/lib/_generic_linux_compilation_build.sh.bash
@@ -1,4 +1,8 @@
 # Generate a build.sh file with the contents of the generic compilation
-# SCRIPT_DIR needed
+# SCRIPT_DIR needed. There is value in having the compilation code
+# independent (to be run) in its own file and having this to generate a
+# build.sh (not run at the moment) with the compilation code (vars expanded)
 
-cp ${SCRIPT_DIR}/lib/_generic_linux_compilation.bash build.sh
+cat > build.sh <<- EOF
+$(envsubst ${SCRIPT_DIR}/lib/_generic_linux_compilation.bash)
+EOF
