a12a1dc11d260fdc4e580a4933b51089584e9d7a
Jose Luis Rivero <jrivero@osrfoundation.org>
2016-10-24 23:23:34 +0200

    Fix change directory

--- jenkins-scripts/docker/srcsim-install-test-job.bash
+++ jenkins-scripts/docker/srcsim-install-test-job.bash
@@ -29,6 +29,7 @@ export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd641
 
 cd /tmp
 git clone https://github.com/ihmcrobotics/ihmc-open-robotics-software
+cd ihmc-open-robotics-software
 git checkout develop
 ./gradlew :Valkyrie:deployLocal1
 cd /opt/ros/indigo/share/ihmc_ros_java_adapter
