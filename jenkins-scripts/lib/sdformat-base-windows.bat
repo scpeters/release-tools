ab078be5be31410e37311f09139c551db10efb90
Jose Luis Rivero <jrivero@osrfoundation.org>
2017-03-17 23:27:31 +0100

    Use SearchForStuff to avoid add logic to the script for ign-math

--- jenkins-scripts/lib/sdformat-base-windows.bat
+++ jenkins-scripts/lib/sdformat-base-windows.bat
@@ -21,13 +21,18 @@
 findstr /r "set.*SDF_MAJOR_VERSION " %WORKSPACE%\sdformat\CMakeLists.txt > version.txt
 set /p SDF_MAJOR_VERSION=<version.txt
 set SDF_MAJOR_VERSION=%SDF_MAJOR_VERSION:~23,24%
-echo %SDF_MAJOR_VERSION%
 
 @if "%SDF_MAJOR_VERSION%" == "5" (
   set IGNMATH_BRANCH="ign-math3"
 else
   set IGNMATH_BRANCH="ign-math2"
 )
+
+findstr /r "set(IGNITION-MATH_REQUIRED_MAJOR_VERSION" %WORKSPACE%\sdformat\cmake\SearchForStuff.cmake > version.txt
+set /p IGN_MATH_REQUIRED_VERSION=<version.txt
+set IGN_MATH_REQUIRED_VERSION=%IGN_MATH_REQUIRED_VERSION:~41,42%
+echo %IGN_MATH_REQUIRED_VERSION%
+set IGNMATH_BRANCH="ign-math%IGN_MATH_REQUIRED_VERSION%"
 
 set win_lib=%SCRIPT_DIR%\lib\windows_library.bat
 set TEST_RESULT_PATH="%WORKSPACE%\test_results"
