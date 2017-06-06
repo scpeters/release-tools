8bd97de38f81253e9912b35d3e1600c00eb05b72
Jose Luis Rivero <jrivero@osrfoundation.org>
2017-06-06 18:01:10 +0200

    ign-math3 as dependency

--- jenkins-scripts/ign_common-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_common-default-devel-windows-amd64.bat
@@ -5,6 +5,6 @@ call "%SCRIPT_DIR%/lib/windows_configuration.bat"
 set VCS_DIRECTORY=ign-common
 set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
-set DEPEN_PKGS=FreeImage-vc12-x64-release-debug.zip dlfcn-win32-vc12-x64-release-debug.zip
+set DEPEN_PKGS=FreeImage-vc12-x64-release-debug.zip dlfcn-win32-vc12-x64-release-debug.zip ign-math3.zip
 
 call "%SCRIPT_DIR%/lib/project-default-devel-windows.bat"
