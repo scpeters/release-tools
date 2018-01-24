eac2851c6dca35d1bc20c4a943ee4ef0a310ab62
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-01-24 01:32:00 +0100

    Fix link to libblas

--- jenkins-scripts/docker/lib/debbuild-base.bash
+++ jenkins-scripts/docker/lib/debbuild-base.bash
@@ -150,7 +150,7 @@ echo '# END SECTION'
 
 echo '# BEGIN SECTION: install build dependencies'
 # Workaround for simbody bug with libblas in Artful
-ln -s /etc/alternatives/libblas.so /usr/lib/libblas.so
+ln -s /usr/lib/x86_64-linux-gnu/blas/libblas.so /usr/lib/libblas.so
 mk-build-deps -r -i debian/control --tool 'apt-get --yes -o Debug::pkgProblemResolver=yes -o  Debug::BuildDeps=yes'
 echo '# END SECTION'
 
