85a790e1c6d792776a09a4c9d281f3e607163a27
Steve Peters <scpeters@openrobotics.org>
2018-05-01 14:59:11 -0700

    set KEEP_WORKSPACE=true for ign-msgs windows build

--- jenkins-scripts/ign_msgs-default-devel-windows-amd64.bat
+++ jenkins-scripts/ign_msgs-default-devel-windows-amd64.bat
@@ -3,5 +3,6 @@ set SCRIPT_DIR="%~dp0"
 set VCS_DIRECTORY=ign-msgs
 set PLATFORM_TO_BUILD=x86_amd64
 set IGN_CLEAN_WORKSPACE=true
+set KEEP_WORKSPACE=TRUE
 
 call "%SCRIPT_DIR%/lib/generic-default-devel-windows.bat"
