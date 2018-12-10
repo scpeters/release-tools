5819bc19c49428841ab113ae8fa4826e152189d9
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-12-10 19:47:22 +0100

    Back tto use ros-core

--- jenkins-scripts/lib/dependencies_archive.sh
+++ jenkins-scripts/lib/dependencies_archive.sh
@@ -334,6 +334,7 @@ else
                     python3-colcon-common-extensions \\
                     python-rosdep                    \\
                     python-wstool                    \\
+                    ros-${ROS_DISTRO}-ros-core       \\
                     python-rosinstall                \\
                     python-rospkg                    \\
                     python-vcstools"
