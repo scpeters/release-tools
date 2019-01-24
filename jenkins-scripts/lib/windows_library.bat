a7a096c51b63b822bafec84d9415770658478b62
Jose Luis Rivero <jrivero@osrfoundation.org>
2019-01-24 19:45:42 +0100

    Run setup.bat in the windows colcon builds

--- jenkins-scripts/lib/windows_library.bat
+++ jenkins-scripts/lib/windows_library.bat
@@ -220,6 +220,8 @@ set COLCON_PACKAGE=%1
 :: test build with tests
 call :_colcon_build_cmd "--packages-skip %COLCON_PACKAGE%" " -DBUILD_TESTING=0"
 call :_colcon_build_cmd "--packages-select %COLCON_PACKAGE%" " -DBUILD_TESTING=1"
+:: source the install wokrspace
+.\install\setup.bat
 goto :EOF
 
 :: ##################################
