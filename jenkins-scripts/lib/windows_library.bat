37bd8a986dc74db5920f33a2ef597482ee03ee70
Jose Luis Rivero <jrivero@osrfoundation.org>
2019-02-11 15:02:20 +0100

    Clean up

--- jenkins-scripts/lib/windows_library.bat
+++ jenkins-scripts/lib/windows_library.bat
@@ -222,10 +222,7 @@ set COLCON_PACKAGE=%1
 call :_colcon_build_cmd "--packages-skip %COLCON_PACKAGE%" " -DBUILD_TESTING=0"
 call :_colcon_build_cmd "--packages-select %COLCON_PACKAGE%" " -DBUILD_TESTING=1"
 :: source the install wokrspace
-echo %PATH%
-echo ----------- run setup.bat --------------
-call ".\install\setup.bat" || echo "error in setup.bat"
-echo %PATH%
+call ".\install\setup.bat" || goto :error
 goto :EOF
 
 :: ##################################
