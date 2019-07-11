398a4e3f211c753fe24ee83dd9798b94eebfbe0e
Steve Peters <scpeters@openrobotics.org>
2019-07-10 17:08:20 -0700

    ign-physics: use prerelease debs

--- jenkins-scripts/docker/ign_physics-compilation.bash
+++ jenkins-scripts/docker/ign_physics-compilation.bash
@@ -15,7 +15,7 @@ if [[ -z ${DISTRO} ]]; then
 fi
 
 export BUILDING_SOFTWARE_DIRECTORY="ign-physics"
-export BUILDING_JOB_REPOSITORIES="stable"
+export BUILDING_JOB_REPOSITORIES="stable prerelease"
 export BUILDING_PKG_DEPENDENCIES_VAR_NAME="IGN_PHYSICS_DEPENDENCIES"
 
 export USE_GCC8=true
