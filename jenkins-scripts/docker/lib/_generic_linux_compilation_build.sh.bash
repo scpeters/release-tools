71d5555d83a6495a23563eaa6d14e0a1551212fd
Jose Luis Rivero <jrivero@osrfoundation.org>
2020-04-02 18:53:55 +0200

    pass MAKE_JOBS var

--- jenkins-scripts/docker/lib/_generic_linux_compilation_build.sh.bash
+++ jenkins-scripts/docker/lib/_generic_linux_compilation_build.sh.bash
@@ -67,7 +67,7 @@ cat >> build.sh << DELIM_BUILD_DEPS
     GENERIC_ENABLE_TESTS=false
     SOFTWARE_DIR=$dep
     cd $WORKSPACE
-    . ${SCRIPT_DIR}/lib/_generic_linux_compilation.bash ${SCRIPT_DIR}
+    . ${SCRIPT_DIR}/lib/_generic_linux_compilation.bash ${SCRIPT_DIR} \${MAKE_JOBS}
     cd $WORKSPACE &&  rm -fr $WORKSPACE/build
 DELIM_BUILD_DEPS
   fi
